ea3111a9e5ee1164e74e0a1efd3921381f343cf9
Kayk Magalhaes <kaykapple1@gmail.com>
2026-09-12 13:18:12 -0300

    deploy config

--- src/main/java/com/kayk/healthscheduler/infra/security/SecurityConfigurations.java
+++ src/main/java/com/kayk/healthscheduler/infra/security/SecurityConfigurations.java
@@ -27,6 +27,15 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs/",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+
+                        ).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/doctors").hasRole("ADMIN")
